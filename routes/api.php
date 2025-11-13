5cbc05b5740137a7a8dba131049209a83fd11d16
Tushar Sawant <94215556+tusharsawant04@users.noreply.github.com>
2025-11-13 17:55:40 +0530

    fourth

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('users', [UserController::class, 'index']);
     Route::post('users', [UserController::class, 'store']);
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::put('users/{user}', [UserController::class, 'update']);
